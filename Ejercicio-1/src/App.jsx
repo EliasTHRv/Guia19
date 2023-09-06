a831dd8035aaf1f25a8a608df8abd78fcbc7ed96
Elias Barreto <eliasgobarreto@gmail.com>
2023-09-05 21:36:14 -0300

    oh oh oh it's magic!

---
+++ Ejercicio-1/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+ 
+  const titulo = "Hola amor";
+  const subtitutlo = "Estas muy muy hermosa!!!!";
+
+  return (
+   <div>
+    <h1>{titulo}</h1>
+    <h2>{subtitutlo}</h2>
+   </div>
+  )
+}
+
+export default App
+
